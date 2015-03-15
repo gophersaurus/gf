cd780419ef925c659d774ac5fc1f734aa1a50755
Jack.Spirou <jack.spirou@target.com>
2015-03-15 18:33:35 -0500

    init commit nothing is working yet

---
+++ gf.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"os"
+	"runtime"
+
+	"github.com/codegangsta/cli"
+)
+
+// USAGE
+//
+// gf [global options..] command [command options...]          [arguments...]
+//    -v, -verbose       new     -g="git@github.git"           project
+//                               -git="https://github.com.git"
+//
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	// start a new cli application.
+	app := cli.NewApp()
+
+	// define the cli application metadata.
+	app.Name = "gf"
+	app.Usage = "A CLI tool for starting Gophersaurus projects."
+	app.Version = "0.0.1"
+	app.Author = "Jack Spirou"
+	app.Email = "jack.spirou@me.com"
+
+	// define the list of global options.
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "verbose",
+			Usage: "Verbose Mode: Print stuff as it happens.",
+		},
+	}
+
+	// define the list of commands.
+	app.Commands = []cli.Command{
+		{
+			Name:        "new",
+			Usage:       "Create a new Gophersaurus project.",
+			ShortName:   "new",
+			Description: "Use this command to initalize a new Gophersaurus project.",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "g, git",
+					Usage: "Git: Initalize the project with a git repository and create upstream.",
+				},
+			},
+			Action: func(c *cli.Context) {
+				if err := NewProject(c.Args().First(), c.String("g"), c.GlobalBool("verbose")); err != nil {
+					panic(err)
+				}
+			},
+		},
+	}
+
+	// define the default action.
+	app.Action = func(c *cli.Context) {
+		cli.ShowAppHelp(c)
+	}
+
+	// execute the cli command given.
+	app.Run(os.Args)
+}
---
+++ newproject.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"os"
+	"os/exec"
+	"path"
+)
+
+// NewProject creates a new Gophersaurus project.
+func NewProject(name, git string, verbose bool) error {
+
+	var org string
+
+	// Check if we a name or git url.
+	if len(name) == 0 && len(git) == 0 {
+		return errors.New("Need a project name or git repository URL.")
+	}
+
+	// Extract the name of the project from the git repository URL.
+	if len(name) == 0 && len(git) > 0 {
+		base := path.Base(git)
+		ext := path.Ext(base)
+		name = base[0 : len(base)-len(ext)]
+	}
+
+	// If the git URL was provided, compare the org name to the current directory.
+	// If the two are different, display a warning for the sake of $GOPATH.
+	//
+	// Currently this only supports github.
+	if len(git) > 0 {
+
+		uri := git
+
+		if uri[0:len("git@github.com:")] == "git@github.com:" {
+			uri = uri[len("git@github.com:"):]
+		}
+
+		u, err := url.Parse(uri)
+		if err != nil {
+			return err
+		}
+
+		dir, _ := path.Split(u.Path)
+		if dir[0] == '/' {
+			dir = dir[1:]
+		}
+		dir = path.Clean(org)
+
+		// Get the current working directory.
+		wd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+
+		if wd != dir {
+			fmt.Println("Warning: The current directory might not match the git URL provided.")
+			fmt.Println("Do you want to continue and use " + dir + " instead of " + wd + " for your import paths?")
+			if !askForConfirmation() {
+				os.Exit(1)
+			}
+		}
+
+		org = dir
+	}
+
+	// Clone the Gophersaurus repository.
+	err := exec.Command("git", "clone", "https://git.target.com/gophersaurus/gophersaurus.git").Run()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("cloned https://git.target.com/gophersaurus/gophersaurus.git")
+
+	// Rename gophersaurus directory by project name.
+	err = exec.Command("mv", "gophersaurus", name).Run()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("renamed gophersaurus -> " + name)
+
+	// Find and replace gophersaurus/gophersaurus, with org/name in the entire directory.
+	//find . -type f -print0 | xargs -0 sed -i 's/subdomainA.example.com/subdomainB.example.com/g'
+	err = exec.Command("find", "/"+name, "-type", "f", "-print0", "|", "xargs", "-0", "sed", "-i", "'s/gophersaurus//gophersaurus/"+org+"//"+name+"/g'").Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
